d1c106c94884df844861d2934e79bd7630ad9343
Kamran <kamran.07@icloud>
2023-10-30 16:17:01 +0300

    first commit

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -1,11 +1,2 @@
-package ru.kata.spring.boot_security.demo.service;
-
-import ru.kata.spring.boot_security.demo.entity.Role;
-
-import java.util.List;
-
-public interface RoleService {
-
-    List<Role> findAll();
-    void addRole(Role role);
+package ru.kata.spring.boot_security.demo.service;public interface RoleService {
 }
